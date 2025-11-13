3c3af52236d77f18c291b4a8163ded6ca0c2a80c
saigerutherford <saruther@med.umich.edu>
2025-11-13 23:13:41 +0100

    edit clinica merge script

--- s4_clinica/merge_individual_clinica.sh
+++ s4_clinica/merge_individual_clinica.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-cd /N/project/statadni/BIDS_individual/ # path to Clinica BIDS output directory (where indidividual subject folders are located)
+basepath=/N/project/statadni
+BIDS_indiv=${basepath}/BIDS_individual/ # path to Clinica BIDS output directory (where indidividual subject folders are located)
 
-mkdir -p /N/project/statadni/BIDS_individual/BIDS_all
-mkdir -p /N/project/statadni/BIDS_individual/BIDS_all/conversion_info
-mkdir -p /N/project/statadni/BIDS_individual/BIDS_all/conversion_info/v0
+mkdir -p ${basepath}/BIDS_all
+mkdir -p ${basepath}/BIDS_all/conversion_info
+mkdir -p ${basepath}/BIDS_all/conversion_info/v0
 
-for i in `cat adni_subs.txt`
+for i in `cat ${basepath}/s4_clinica/adni_subs.txt`
 do
     echo "Merging subject ${i} BIDS folder"
-    if [[ -e /N/project/statadni/BIDS_individual/${i} ]]; then
-        cp -r /N/project/statadni/BIDS_individual/${i} /N/project/statadni/BIDS_individual/BIDS_all/sub-${i}
-        if [[ ! -e /N/project/statadni/BIDS_individual/BIDS_all/${i}/conversion_info/v0/fmri_paths.tsv ]]; then
-            cat /N/project/statadni/BIDS_individual/${i}/conversion_info/v0/fmri_paths.tsv >> /N/project/statadni/BIDS_individual/BIDS_all/conversion_info/v0/fmri_paths.tsv
+    if [[ -e ${BIDS_indiv}/${i} ]]; then
+        cp -r ${BIDS_indiv}/${i} ${basepath}/BIDS_all/sub-${i}
+        if [[ ! -e ${BIDS_indiv}/${i}/conversion_info/v0/fmri_paths.tsv ]]; then
+            cat ${BIDS_indiv}/${i}/conversion_info/v0/fmri_paths.tsv >> ${basepath}/BIDS_all/conversion_info/v0/fmri_paths.tsv
         fi
-        if [[ ! -e /N/project/statadni/BIDS_individual/BIDS_all/${i}/conversion_info/v0/t1w_paths.tsv ]]; then
-            cat /N/project/statadni/BIDS_individual/${i}/conversion_info/v0/t1w_paths.tsv >> /N/project/statadni/BIDS_individual/BIDS_all/conversion_info/v0/t1w_paths.tsv
+        if [[ ! -e ${BIDS_indiv}/${i}/conversion_info/v0/t1w_paths.tsv ]]; then
+            cat ${BIDS_indiv}/${i}/conversion_info/v0/t1w_paths.tsv >> ${basepath}/BIDS_all/conversion_info/v0/t1w_paths.tsv
         fi
-        if [[ ! -e /N/project/statadni/BIDS_individual/BIDS_all/${i}/conversion_info/v0/flair_paths.tsv ]]; then
-            cat /N/project/statadni/BIDS_individual/${i}/conversion_info/v0/flair_paths.tsv >> /N/project/statadni/BIDS_individual/BIDS_all/conversion_info/v0/flair_paths.tsv
+        if [[ ! -e ${BIDS_indiv}/${i}/conversion_info/v0/flair_paths.tsv ]]; then
+            cat ${BIDS_indiv}/${i}/conversion_info/v0/flair_paths.tsv >> ${basepath}/BIDS_all/conversion_info/v0/flair_paths.tsv
         fi
-        if [[ ! -e /N/project/statadni/BIDS_individual/BIDS_all/${i}/conversion_info/v0/participants.tsv ]]; then
-            cat /N/project/statadni/BIDS_individual/${i}/conversion_info/v0/participants.tsv >> /N/project/statadni/BIDS_individual/BIDS_all/conversion_info/v0/participants.tsv
+        if [[ ! -e ${BIDS_indiv}/${i}/conversion_info/v0/participants.tsv ]]; then
+            cat ${BIDS_indiv}/${i}/conversion_info/v0/participants.tsv >> ${basepath}/BIDS_all/conversion_info/v0/participants.tsv
         fi
     else
         echo "Subject ${i} BIDS folder not found, skipping."
     fi
+done
+
